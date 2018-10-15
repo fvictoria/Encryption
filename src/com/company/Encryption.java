6b722e4d62b8fad394a6add82401f2a4d4531ed0
fvictoria <felipevictoria630@yahoo.com>
2018-10-15 11:03:03 -0400

    Initial commit

---
+++ src/com/company/Encryption.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by fv799 on 10/15/18.
+ */
+public class Encryption {
+
+    public String Encryp;
+    public Encryption (String hiddenM)
+    {
+        Encryp = hiddenM;
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter string");
+        Scanner kbReader = new Scanner(System.in);
+        String inPut = kbReader.nextLine();
+
+
+
+    }
+}
